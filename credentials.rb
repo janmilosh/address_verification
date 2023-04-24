bbc087f0c5adb5638846748077d10d2517d9ffe4
Jan Milosh <janmilosh@gmail.com>
2023-04-23 20:01:02 -0400

    Update credentials file

--- credentials.rb
+++ credentials.rb
@@ -1,5 +1,6 @@
 # Assuming you have an account with smarty.com...
-# You can find your Secret Keys at https://smarty.com/accounts/keys
+# You can find your Secret Keys at https://www.smarty.com/account/keys
+# You can find your License at https://www.smarty.com/account/subscriptions
 
 AUTH_ID = ''
 AUTH_TOKEN = ''
